be2a76c606d89575f03e342fe96baa0bc2e19282
craitys <erykslezak@outlook.ie>
2022-08-07 23:31:22 +0000

    Added buttons for quantity input

--- products/models.py
+++ products/models.py
@@ -21,7 +21,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     short_description = models.TextField()
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
