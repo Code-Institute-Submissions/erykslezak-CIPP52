c8ace43b03692e2fe5767c57c04e23e7cb7d3dc7
craitys <erykslezak@outlook.ie>
2022-08-07 16:45:18 +0000

    Add products on index page that are sorted by newest product

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from products.models import Product
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    products = Product.objects.all().order_by('-id')
+    return render(request, 'home/index.html', {'products': products})
