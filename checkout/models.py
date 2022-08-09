9b8794b9556388a4871ba3e54305661777692b9e
craitys <erykslezak@outlook.ie>
2022-08-09 19:59:31 +0000

    Add extended userprofile app

--- checkout/models.py
+++ checkout/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from products.models import Product
 
 from django_countries.fields import CountryField
-
+from userprofile.models import UserProfile
 
 def increment_order_number():
     last_order = Order.objects.all().order_by('id').last()
@@ -22,6 +22,8 @@ def increment_order_number():
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, default=increment_order_number, null=True, blank=True, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
---
+++ userprofile/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def profiles(request):
+    """ Display the user's profile. """
+
+    template = 'profiles/profiles.html'
+    context = {
+    }
+
+    return render(request, template, context)
