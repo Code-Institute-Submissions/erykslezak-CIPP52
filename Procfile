0584a9dfcec416fde156957dad7553838d42dc8f
craitys <erykslezak@outlook.ie>
2022-08-10 19:19:27 +0000

    First deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn aquaria_supplies.wsgi:application
--- aquaria_supplies/settings.py
+++ aquaria_supplies/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&i8u)+ylh0uee)42a5ee@go@9r66sfqe#gqp+hqmkbcdn(v(65
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aquaria-supplies.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,16 +118,17 @@ WSGI_APPLICATION = 'aquaria_supplies.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
