632feec74f826d990345ea766aeb23a44658828f
craitys <erykslezak@outlook.ie>
2022-08-10 22:57:21 +0000

    Fixed profile url link and profile.css not loading in deployment

--- aquaria_supplies/urls.py
+++ aquaria_supplies/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
-    path('profiles/', include('userprofile.urls')),
+    path('profile/', include('userprofile.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
