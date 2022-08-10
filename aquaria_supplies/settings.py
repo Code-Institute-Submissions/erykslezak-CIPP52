1257df1021b2117b9587f7fe6eed0a9b75fe52f1
craitys <erykslezak@outlook.ie>
2022-08-10 19:26:06 +0000

    Changed secret key and debug

--- aquaria_supplies/settings.py
+++ aquaria_supplies/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&i8u)+ylh0uee)42a5ee@go@9r66sfqe#gqp+hqmkbcdn(v(65'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['aquaria-supplies.herokuapp.com', 'localhost']
 
